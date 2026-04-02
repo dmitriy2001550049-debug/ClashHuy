19df8632bc473e860cf3b790de53bc1caf9ddb0f
dmitriy2001550049-debug <dmitriy2001550049@gmail.com>
2026-04-02 19:45:32 +0300

    Create misnap_init.sh

---
+++ scripts/misnap_init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copyright (C) Juewuy
+
+clashdir=/data/clash
+profile=/etc/profile
+
+#h初始化环境变量
+sed -i "/alias clash/d" $profile 
+sed -i "/export clashdir/d" $profile 
+echo "alias clash=\"$clashdir/clash.sh\"" >> $profile 
+echo "export clashdir=\"$clashdir\"" >> $profile 
+#设置init.d服务并启动clash服务
+ln -sf $clashdir/clashservice /etc/init.d/clash
+chmod 755 /etc/init.d/clash
+
+if [ ! -f $clashdir/.dis_startup ];then
+	log_file=`uci get system.@system[0].log_file`
+	while [ "$i" -lt 10 ];do
+		sleep 5
+		[ -n "$(grep 'init complete' $log_file)" ] && i=10 || i=$((i+1))
+	done
+	/etc/init.d/clash start
+	/etc/init.d/clash enable
+fi
